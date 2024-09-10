9204aee7e6030c30ae03dee0cf01ae429082f7da
apisit ketkawe <aogcici555@gmail.com>
2024-09-10 23:21:27 +0700

    update export_doc

--- src/app/export/external/page.tsx
+++ src/app/export/external/page.tsx
@@ -1,16 +1,15 @@
 // import Image from "next/image";
 import Navbar from "../../componente/Navbar";
-
-
-
+import SearchableTable from "../../componente/SearchableTable";
+// import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <main>
-     < Navbar/>
-    
+      <Navbar />
+      {/* <Button>Click me</Button> */}
+      <h1 style={{ padding: "20px" }}>ชื่อวิจัย การพัฒนาระบบการจัดเก็บข้อมูลหนังสือราชการ</h1>
+      <SearchableTable />
     </main>
-    
   );
 }
-
