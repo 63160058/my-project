736ca46935fb23c6d19cc5584b7b251eee0fc959
63160058 <63160058@go.buu.ac.th>
2024-10-26 19:48:40 +0700

    update

---
+++ prisma/migrations/20241026124431_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `L_num` on the `officialletter` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `officialletter` DROP COLUMN `L_num`;
